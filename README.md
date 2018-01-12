b26be64c87089972c326a1221124279d132eb16e
keep-looking <8t9587+16br9e37xeuko@sharklasers.com>
2018-01-12 23:06:03 +0900

    1st

---
+++ README.md
@@ -0,0 +1,35 @@
+[![Build Status](https://travis-ci.org/AidosKuneen/numcpu.svg?branch=master)](https://travis-ci.org/AidosKuneen/numcpu)
+[![GoDoc](https://godoc.org/github.com/AidosKuneen/numcpu?status.svg)](https://godoc.org/github.com/AidosKuneen/numcpu)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/AidosKuneen/numcpu/master/LICENSE)
+
+
+NumcCPU
+=====
+
+## Overview
+
+NumCPU returns the number of logical CPUs usable by the current process.
+
+Android doesn't return max number of CPUs by runtime.NumCPU(),
+but just returns number of "working" CPUs.
+So this library checks sysfs directory directly for linux, and just returns runtime.NumCPU() for others.
+
+## Requirements
+
+* git
+* go
+
+are required to compile.
+
+
+## Install
+    $ go get -u github.com/AidosKuneen/numcpu
+
+
+## Usage
+
+```go
+	import "github.com/AidosKuneen/numcpu"
+	n:=numcpu.NumCPU()
+```
+
---
+++ numcpu.go
@@ -0,0 +1,57 @@
+// Copyright (c) 2017 Aidos Developer
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package numcpu
+
+import (
+	"os"
+	"runtime"
+	"strconv"
+	"strings"
+)
+
+// NumCPU returns the number of logical CPUs usable by the current process.
+//Android doesn't return max CPU by runtime.NumCPU(), but just returns number of working CPUs.
+//So this checks sysfs directly for linux.
+func NumCPU() int {
+	dir := "/sys/devices/system/cpu/"
+	f, err := os.Open(dir)
+	if err != nil {
+		return runtime.NumCPU()
+	}
+	list, err := f.Readdir(-1)
+	f.Close()
+	if err != nil {
+		return runtime.NumCPU()
+	}
+	n := 0
+	for _, l := range list {
+		name := l.Name()
+		if l.IsDir() && strings.HasPrefix(name, "cpu") {
+			if _, err := strconv.Atoi(name[3:]); err == nil {
+				n++
+			}
+		}
+	}
+	if n == 0 {
+		return runtime.NumCPU()
+	}
+	return n
+}
